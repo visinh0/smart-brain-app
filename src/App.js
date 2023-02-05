c7eb91b2a6306355c5f60e038cee4975acf683fd
visinh0 <dmitriibd90@gmail.com>
2023-02-05 13:15:52 +0200

    front-end

--- src/App.js
+++ src/App.js
@@ -1,25 +1,157 @@
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import Navigation from './components/Navigation/Navigation';
+import Signin from './components/Signin/Signin';
+import Register from './components/Register/Register';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
+import Logo from './components/Logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import ParticlesBg from 'particles-bg';
 
-function App() {
+
+let config = {
+      num: [4, 7],
+      rps: 0.1,
+      radius: [45, 70],
+      life: [1.5, 3],
+      v: [2, 3],
+      tha: [-40, 40],
+      // body: "./img/icon.png", // Whether to render pictures
+      // rotate: [0, 20],
+      alpha: [0.6, 0],
+      scale: [1, 0.1],
+      position: "center", // all or center or {x:1,y:1,width:100,height:100}
+      color: ["random", "#ff0000"],
+      cross: "dead", // cross or bround
+      random: 15,  // or null,
+      g: 5,    // gravity
+      // f: [2, -1], // force
+      onParticleUpdate: (ctx, particle) => {
+          ctx.beginPath();
+          ctx.rect(particle.p.x, particle.p.y, particle.radius * 2, particle.radius * 2);
+          ctx.fillStyle = particle.color;
+          ctx.fill();
+          ctx.closePath();
+      }
+    };
+
+const initialState = {
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+    id: '',
+    name: '',
+    email: '',
+    entries: 0,
+    joined: ''
+}
+}
+class App extends Component {
+constructor() {
+  super();
+  this.state = initialState;
+  }
+
+
+loadUser = (data) => {
+  this.setState({user: {
+    id: data.id,
+    name: data.name,
+    email: data.email,
+    entries: data.entries,
+    joined: data.joined
+  }})
+}
+
+
+calculateFaceLocation = (data) => {
+  const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+  const image = document.getElementById('inputimage');
+  const width = Number(image.width);
+  const height = Number(image.height);
+  return {
+    leftCol: clarifaiFace.left_col * width,
+    topRow: clarifaiFace.top_row * height,
+    rightCol: width - (clarifaiFace.right_col * width),
+    bottomRow: height - (clarifaiFace.bottom_row * height)
+  }
+}
+
+displayFaceBox = (box) => {
+  this.setState({box: box});
+}
+
+onInputChange = (event) => {
+  this.setState({input: event.target.value});
+}
+
+onButtonSubmit = () => {
+  this.setState({imageUrl: this.state.input});
+    fetch('http://localhost:3000/imageurl', {
+        method: 'post',
+        headers: {'Content-Type': 'application/json'},
+        body: JSON.stringify({
+        input: this.state.input
+        })
+      })
+    .then(response => response.json())
+      .then(response => {
+      if (response) {
+        fetch('http://localhost:3000/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+          id: this.state.user.id
+          })
+        })
+        .then(response => response.json())
+        .then(count => {
+          this.setState(Object.assign(this.state.user, {entries: count}))
+          })
+          .catch(console.log)
+        }
+        this.displayFaceBox(this.calculateFaceLocation(response))
+      })
+        .catch(err => console.log(err));
+     }
+
+onRouteChange = (route) => {
+  if (route === 'signout') {
+    this.setState(initialState)
+  } else if (route === 'home') {
+    this.setState({isSignedIn: true})
+  }
+  this.setState({route: route});
+}
+render() {
+  const { isSignedIn, route, imageUrl, box } = this.state;
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <ParticlesBg className='particles' type="custom" config={config} bg={true} />
+      <Navigation isSignedIn={isSignedIn} onRouteChange={this.onRouteChange}/>
+      { route === 'home' 
+       ?  <div>
+          <Logo />
+          <Rank name={this.state.user.name} entries={this.state.user.entries}  />
+          <ImageLinkForm 
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+          />
+          <FaceRecognition box={box} imageUrl={imageUrl} />
+          </div>
+      : (
+        route === 'signin' 
+        ? <Signin loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange} />
+        )
+    }
+    
     </div>
   );
 }
-
+}
 export default App;
